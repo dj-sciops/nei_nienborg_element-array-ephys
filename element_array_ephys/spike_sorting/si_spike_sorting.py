8c444b74ddedeeb5d17b6fb12b9b1730a90637a3
Thinh Nguyen <thinh@datajoint.com>
2024-10-01 15:39:57 -0500

    Added fix to make phy export overwrite if the phy folder already exists

--- element_array_ephys/spike_sorting/si_spike_sorting.py
+++ element_array_ephys/spike_sorting/si_spike_sorting.py
@@ -382,6 +382,7 @@ class SIExport(dj.Computed):
                 sorting_analyzer=sorting_analyzer,
                 output_folder=analyzer_output_dir / "phy",
                 use_relative_path=True,
+                remove_if_exists=True,
                 **job_kwargs,
             )
 
